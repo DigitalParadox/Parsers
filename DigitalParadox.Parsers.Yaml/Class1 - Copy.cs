2eefb5c85671a62a3048ad24564646ed7c0f9d99
Chris McGrath <chris.mcgrath@digitalparadox.ca>
2017-07-23 03:00:00 -0400

    initial commit

---
+++ DigitalParadox.Parsers.Yaml/Class1 - Copy.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SharpYaml.Serialization;
+
+namespace DigitalParadox.Parsers.Yaml
+{
+    public class SharpYamlParser : IYamlParser
+    {
+        private readonly Serializer _serializer;
+
+        public SharpYamlParser(Serializer serializer)
+        {
+            _serializer = serializer;
+        }
+
+        public string Serialize(object data)
+        {
+            var yaml = _serializer.Serialize(data);
+            return yaml;
+        }
+
+        public T Deserialize<T>(string yaml)
+        {
+            var obj = _serializer.Deserialize<T>(yaml);
+            return obj;
+        }
+
+        public object Deserialize(string yaml)
+        {
+            var obj = _serializer.Deserialize(yaml);
+            return obj;
+        }
+    }
+
+    public interface IYamlParser
+    {
+        string Serialize(object data);
+        T Deserialize<T>(string yaml);
+        object Deserialize(string yaml);
+    }
+}
---
+++ DigitalParadox.Parsers.Yaml/YamlUnityExtension.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Practices.Unity;
+using SharpYaml.Serialization;
+
+namespace DigitalParadox.Parsers.Yaml
+{
+    public class YamlUnityExtension : UnityContainerExtension
+    {
+        protected override void Initialize()
+        {
+
+
+            Container.RegisterInstance<IObjectFactory>(new LambdaObjectFactory(
+                type =>
+                {
+                    if (Container.Registrations.Any(registration => registration.RegisteredType == type))
+                    {
+                        return Container.Resolve(type);
+                    }
+                    else
+                    {
+                        return null;
+                    }
+                },
+                new DefaultObjectFactory()));
+
+            Container.RegisterInstance(new SerializerSettings()
+            {
+                ObjectFactory = Container.Resolve<IObjectFactory>()
+            });
+
+            Container.RegisterType<Serializer>();
+            Container.RegisterType<IYamlParser, SharpYamlParser>();
+        }
+    }
+}
